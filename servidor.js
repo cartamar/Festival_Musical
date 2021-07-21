e49dc8482843d6d9b203814c813df63738067b32
cartamar <erikivan14@gmail.com>
2021-07-21 18:43:44 -0500

    sin node_modules

---
+++ servidor.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const server = express();
+
+const port = 3000;
+
+server.set('view engine', 'ejs');
+
+server.use(express.static(__dirname + "/public"));
+
+server.get('/', (req, res) => {
+    res.send('Festival Musical');
+});
+
+server.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + "/public/404.html")
+})
+
+server.listen(port, () => {
+    console.log('Servidor Musical...', port);
+});
