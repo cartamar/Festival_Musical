a75f21184205e524f10d1fa10579404ea6db1126
cartamar <erikivan14@gmail.com>
2021-07-23 22:36:43 -0500

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,40 @@
+const {series, parallel, src, dest, watch} = require('gulp');
+const sass = require('gulp-dart-sass');
 
 
-function compilarSASS(){
-    console.log("compilando");
+function css( ){
+     return src('src/scss/app.scss')
+     .pipe( sass() )
+     .pipe( dest('./public/css'));
+};
+
+function cssMin( ){
+     return src('src/scss/app.scss')
+     .pipe( sass({
+          outputStyle: 'compressed'
+     }) )
+     .pipe( dest('./public/css'));
+};
+
+function watchArchivos () {
+     watch('src/scss/app.scss', css);
 }
+
+// function JavaScript( done ) {
+//     console.log('Compilando JavaScript');
+
+//     done();
+// }
+
+// function minHTML( done ) {
+//     console.log('Minificando HTML');
+
+//     done();
+// }
+
+exports.css = css;
+exports.cssMin = cssMin;
+exports.watchArchivos = watchArchivos;
+// exports.JavaScript = JavaScript;
+// //exports.default = series(CSS, JavaScript, minHTML);
+// exports.default = parallel(CSS, JavaScript, minHTML);
