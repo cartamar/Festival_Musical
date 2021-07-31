49543e3784bb7cbb5867a8c8b0fc266e77ef758d
cartamar <erikivan14@gmail.com>
2021-07-30 21:41:55 -0500

    Minificado de imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const {series, parallel, src, dest, watch} = require('gulp');
 const sass = require('gulp-dart-sass');
+const imagemin = require('gulp-imagemin')
 
 
 function css( ){
@@ -15,6 +16,12 @@ function cssMin( ){
      }) )
      .pipe( dest('./public/css'));
 };
+
+function imagenes() {
+     return src('src/img/**/*')
+          .pipe(imagemin())
+          .pipe(dest('./public/img'))
+}
 
 function watchArchivos () {
      watch('src/scss/**/*.scss', css);
@@ -34,6 +41,7 @@ function watchArchivos () {
 
 exports.css = css;
 exports.cssMin = cssMin;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
 // exports.JavaScript = JavaScript;
 // //exports.default = series(CSS, JavaScript, minHTML);
